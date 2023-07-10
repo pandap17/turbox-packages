674da8954f689c66dee26e74e9ca64f91aab7dc6
pandap17 <135696214+pandap17@users.noreply.github.com>
2023-07-10 16:14:02 -0500

    Update AppBuilder.py

--- appbuilder/AppBuilder.py
+++ appbuilder/AppBuilder.py
@@ -1,7 +1,7 @@
 import base64
 import json
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox, ttk
 
 def file_to_data_url(file_path):
     with open(file_path, "rb") as file:
@@ -17,28 +17,60 @@ def get_mime_type(file_path):
 def prompt_for_files():
     root = tk.Tk()
     root.withdraw()
-    file_paths = filedialog.askopenfilenames()
+    file_paths = filedialog.askopenfilenames(filetypes=[("Sprite Files", "*.sprite3")])
     return list(file_paths)
+
+def prompt_for_extensions(file_paths):
+    def save_extensions():
+        extensions = extension_text.get("1.0", "end").strip().split("\n")
+        root.destroy()
+        process_files_and_extensions(file_paths, extensions)
+
+    root = tk.Tk()
+    root.title("Extension Links")
+    root.resizable(False, False)
+    root.geometry("400x300")
+
+    label = tk.Label(root, text="Paste the links to your extensions (one link per line):")
+    label.pack(pady=10)
+
+    extension_text = tk.Text(root, height=10, width=40)
+    extension_text.pack(pady=5)
+
+    save_button = ttk.Button(root, text="Save", command=save_extensions)
+    save_button.pack(pady=10)
+
+    root.mainloop()
+
+def process_files_and_extensions(file_paths, extensions):
+    # Convert each file to a data URL
+    data_urls = []
+    for file_path in file_paths:
+        data_url = file_to_data_url(file_path)
+        data_urls.append('import sprite')
+        data_urls.append(data_url)
+
+    # Append extension links to the data URLs if extensions are provided
+    if extensions:
+        for extension in extensions:
+            data_urls.append('import extension')
+            data_urls.append(extension.strip())
+
+    # Construct the JSON output
+    json_output = json.dumps(data_urls + ["stop"])
+
+    # Ask the user for the filename
+    filename = filedialog.asksaveasfilename(
+        defaultextension=".tpkg", filetypes=[("Turbox Package", "*.tpkg")]
+    )
+    if filename:
+        # Save the JSON output to a file
+        with open(filename, "w") as file:
+            file.write(json_output)
+        messagebox.showinfo("Build Output", f"Build output saved to {filename}")
 
 # Prompt the user to select files using the Windows Explorer
 file_paths = prompt_for_files()
 
-# Convert each file to a data URL
-data_urls = []
-for file_path in file_paths:
-    data_url = file_to_data_url(file_path)
-    data_urls.append('import sprite')
-    data_urls.append(data_url)
-
-# Construct the JSON output
-json_output = json.dumps(data_urls + ["stop"])
-
-# Ask the user for the filename
-filename = input("What would you like to save your app as? ")
-
-# Save the JSON output to a file
-output_file_path = f"{filename}.osp"
-with open(output_file_path, "w") as file:
-    file.write(json_output)
-
-print(f"Build output saved to {output_file_path}")
+# Prompt the user for extension links
+prompt_for_extensions(file_paths)
